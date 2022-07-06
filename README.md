a72d7cf5d76c9f5d4e2ba77f75d2e56f7436107c
Ariel Favaro <arieltainan@gmail.com>
2022-07-06 17:01:04 -0300

    first draft

---
+++ README.md
@@ -0,0 +1,20 @@
+# Another Github Badges
+
+## Available Badges
+...
+
+
+## TODO 📌
+- Add database
+- Add throttling to endpoints
+- Add more badges
+
+## Getting Started
+
+```bash
+npm install
+```
+
+```bash
+npm run dev
+```
---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const routerBadges = express.Router();
+const { visits } = require('./lib/badges');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.redirect('https://github.com/arielfavaro');
+});
+
+routerBadges.use((req, res, next) => {
+    res.set('Content-type', 'image/svg+xml;charset=utf-8');
+    next();
+});
+
+routerBadges.get('/', (req, res) => {
+    res.set('Content-type', 'text/html;chartset=utf-8');
+    res.status(400).send('Whoops 👻');
+});
+
+routerBadges.get('/visits/:user/:repo', (req, res) => {
+    // TODO implement, add validation
+    // const { user, repo } = req.params;
+    res.send(visits(123));
+});
+
+app.use('/badges', routerBadges);
+
+app.listen(port, () => console.log(`App running on port ${port} 🚀`));
