c59ba7cedb3d66f04a6c8cec0f37991602d86691
Ariel Favaro <arieltainan@gmail.com>
2022-07-08 13:00:37 -0300

    added cron to log github api rate usage

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const routerBadges = express.Router();
+const cron = require('cron');
+const chalk = require('chalk');
 const { visits } = require('./lib/badges');
 const apiGithub = require('./services/apiGithub');
 
@@ -30,3 +32,25 @@ routerBadges.get('/visits/:user/:repo', (req, res) => {
 app.use('/badges', routerBadges);
 
 app.listen(port, () => console.log(`App running on port ${port} 🚀`));
+
+
+cron.job('*/10 * * * *', async () => {
+    console.log(chalk.white.bgBlue.bold('GitHub api rate usage'));
+    try {
+        const { data } = await apiGithub.get('/rate_limit');
+        console.log(
+            `
+            Limit: ${data.rate.limit}
+            Used: ${data.rate.used}
+            Remaining: ${data.rate.remaining}
+            Reset at: ${data.rate.reset}
+            `
+        );
+
+    } catch (error) {
+        console.error('Error fetching GitHub api usage');
+        // TODO
+        console.error(error.response.status);
+        console.error(error.response.statusText);
+    }
+}).start();
