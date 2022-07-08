0751afec8498f950590207934113e90cc12d3cdb
Ariel Favaro <arieltainan@gmail.com>
2022-07-08 13:10:38 -0300

    draft

--- src/server.js
+++ src/server.js
@@ -13,20 +13,28 @@ app.get('/', (req, res) => {
     res.redirect('https://github.com/arielfavaro');
 });
 
-routerBadges.use((req, res, next) => {
-    res.set('Content-type', 'image/svg+xml;charset=utf-8');
-    next();
-});
-
 routerBadges.get('/', (req, res) => {
     res.set('Content-type', 'text/html;chartset=utf-8');
     res.status(400).send('Whoops 👻');
 });
 
-routerBadges.get('/visits/:user/:repo', (req, res) => {
-    // TODO implement, add validation
-    // const { user, repo } = req.params;
-    res.send(visits(123));
+routerBadges.get('/visits/:user/:repo', async (req, res) => {
+
+    const { user, repo } = req.params;
+
+    try {
+        const { data } = await apiGithub.get(`/repos/${user}/${repo}`);
+        const { id, full_name, url } = data;
+
+        // TODO implement
+        // TODO save repo id as key instead of user-repo as slug on database?
+        console.log(id);
+
+    } catch (error) {
+        return res.status(400).send('Whoops 👻');
+    }
+
+    res.type('svg').send(visits(123));
 });
 
 app.use('/badges', routerBadges);
